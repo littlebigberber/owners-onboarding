2b0e2989c15adb9da28c66419d030ddf2c6c9257
littlebigberber <111088107+littlebigberber@users.noreply.github.com>
2025-11-10 06:24:08 +0100

    Add files via upload

---
+++ assets/baselines.js
@@ -0,0 +1,7 @@
+// Inline baselines to avoid fetch()/CORS issues when opening locally.
+window.DRP_BASELINES = {
+  "Gueliz": { "Studio":{"adr":500,"occ":58}, "1BR":{"adr":650,"occ":60}, "2BR":{"adr":800,"occ":55} },
+  "Hivernage": { "Studio":{"adr":600,"occ":60}, "1BR":{"adr":800,"occ":58}, "2BR":{"adr":1000,"occ":55} },
+  "Medina": { "Studio":{"adr":450,"occ":55}, "1BR":{"adr":650,"occ":54}, "2BR":{"adr":900,"occ":52} },
+  "Palmeraie": { "Studio":{"adr":520,"occ":56}, "1BR":{"adr":720,"occ":55}, "2BR":{"adr":950,"occ":53} }
+};
